b3e75bb783300a02a3ca998ce0c17e0220ff8983
adidvar <adidvar@gmail.com>
2025-04-17 13:05:54 +0300

    class Position() finished

--- include/bitboard/color.hpp
+++ include/bitboard/color.hpp
@@ -5,6 +5,11 @@
 namespace bitboard
 {
 
+/**
+* @brief Enumerates the two colors in chess.
+*
+* This enum is used to clearly distinguish between white and black.
+*/
 enum struct Color : int8_t
 {
   kWhite = -1,
